1300bc6e565c40b58792dbb48cabf2de1afff387
Joseph Infante <josephinfante.y@gmail.com>
2022-01-21 09:33:41 -0500

    Adding getters and setters

--- src/main/java/com/backend/todo_list_backend/task/Task.java
+++ src/main/java/com/backend/todo_list_backend/task/Task.java
@@ -5,12 +5,10 @@ import java.time.LocalDateTime;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @Document
-@AllArgsConstructor
 public class Task {
     
     @Id
@@ -20,11 +18,36 @@ public class Task {
     private LocalDateTime updated_at;
 
 
-    public Task(String name, LocalDateTime created_at, LocalDateTime updated_at) {
-        this.name = name;
-        this.created_at = created_at;
-        this.updated_at = updated_at;
+    public String getId() {
+        return this.id;
     }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return this.created_at;
+    }
+
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+
+    public LocalDateTime getUpdated_at() {
+        return this.updated_at;
+    }
+
+    public void setUpdated_at(LocalDateTime updated_at) {
+        this.updated_at = updated_at;
+    }
 
 }
