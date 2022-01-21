27518e9ed595f48ad91e6679ea46747c4351fa68
Joseph Infante <josephinfante.y@gmail.com>
2022-01-21 09:36:53 -0500

    Removing unused package

--- src/main/java/com/backend/todo_list_backend/task/TaskController.java
+++ src/main/java/com/backend/todo_list_backend/task/TaskController.java
@@ -1,9 +1,7 @@
 package com.backend.todo_list_backend.task;
 
 import java.util.List;
-import java.util.Optional;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
