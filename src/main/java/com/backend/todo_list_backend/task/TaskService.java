354cab601a1d050a89392ae134ad0133023d254c
Joseph Infante <josephinfante.y@gmail.com>
2022-01-21 09:34:32 -0500

    Updating Query service

--- src/main/java/com/backend/todo_list_backend/task/TaskService.java
+++ src/main/java/com/backend/todo_list_backend/task/TaskService.java
@@ -1,41 +1,42 @@
 package com.backend.todo_list_backend.task;
 
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TaskService {
 
-    private final TaskRepository taskRepository;
+    @Autowired
+    private TaskRepository taskRepository;
 
-    public TaskService(TaskRepository taskRepository) {
-        this.taskRepository = taskRepository;
-    }
-
-    public void createTask(Task task) {
-        taskRepository.insert(task);
-    }
-
-    public void updateTask(String id, Task task) {
-        Task savedTask = taskRepository.findById(id)
-            .orElseThrow(()-> new RuntimeException(
-                String.format("Cannot find task by ID %s", id)
-            ));
-        taskRepository.save(savedTask);
-    }
-
-    public List<Task> getAllTask() {
+    public List<Task> getAll() {
         return taskRepository.findAll();
     }
 
-    public Optional<Task> getTaskById(String id) {
-        return taskRepository.findById(id);
+    public void createTask(Task task) {
+        task.setCreated_at(LocalDateTime.now());
+        task.setUpdated_at(LocalDateTime.now());
+        taskRepository.save(task);
+    }
+
+    public void updateTask(String id, Task task) {
+        Task taskbd = taskRepository
+            .findById(id)
+                .orElseThrow(() -> new RuntimeException(String.format("No task was found, ID: ", id)));
+        taskbd.setName(task.getName());
+        taskbd.setUpdated_at(LocalDateTime.now());
+        taskRepository.save(taskbd);
+
     }
 
     public void deleteTask(String id) {
-        taskRepository.deleteById(id);
+        Task task = taskRepository
+            .findById(id)
+                .orElseThrow(() -> new RuntimeException(String.format("No task was found, ID: ", id)));
+        taskRepository.delete(task);
     }
     
 }
